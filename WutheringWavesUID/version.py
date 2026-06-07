ee3ae0fe7d18d27d02e0377d4a014663a665719b
eli stewart <3483948113@qq.com>
2026-06-07 21:42:04 +0800

    ✨version_3.4

--- WutheringWavesUID/version.py
+++ WutheringWavesUID/version.py
@@ -1,3 +1,3 @@
-WutheringWavesUID_version = "3.3.0"
-version_start_datetime = "2026-04-30 11:00:00"
-version_end_datetime = "2026-06-08 04:00:00"
+WutheringWavesUID_version = "3.4.0"
+version_start_datetime = "2026-06-08 11:00:00"
+version_end_datetime = "2026-07-10 04:00:00"
